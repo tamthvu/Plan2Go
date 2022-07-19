03452782271fbf34b14f508fa1b799f93e4fa493
Jacky Li <90122922+jackyusa@users.noreply.github.com>
2022-07-18 23:07:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const navBar = document.querySelector('.navBar');
 const car = document.querySelector('.fa-solid fa-car-side');
 const plane = document.querySelector('.fa-solid fa-plane-up');
 const headline = document.querySelector('.headline');
+const icons = document.querySelector('.navOptions');
+const body = document.querySelector('body');
+var inputLocation = ""
 
 const tl = new TimelineMax();
 
 tl.fromTo(slider1, .9, {x: "0%"}, {x: "100%"},"+=.6")
     .fromTo(slider2, .8, {y: "0%"}, {y:"-100%"},"-=.1")
-    .fromTo(navBar, 1, {y: "-100%"}, {y:"0%"});
+    .fromTo(navBar, 1, {y: "-100%"}, {y:"0%"},"-=.1");
 
 var i = 0;
 var txt = 'Plan your next trip ';
@@ -20,7 +23,7 @@ var checker = 0;
 window.onloadstart = wait();
 
 function wait(){
-    setTimeout(typeWriter, 2350);
+    setTimeout(typeWriter, 2450);
 };
 
 function typeWriter() {
@@ -30,3 +33,12 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 };
+
+function darkMode(){
+    body.classList.toggle("dark-mode");
+    icons.classList.toggle("dark-mode-icons");
+}
+
+function saveName(){
+    inputLocation = document.getElementById("search").value;
+}
